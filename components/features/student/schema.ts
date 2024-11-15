65d80a65ec0c164aafb85b1e756ae9e029c29f90
Ivan Lara <ukikalix@gmail.com>
2024-11-15 01:06:31 -0500

    ft/dynamic schema added on student form

--- components/features/student/schema.ts
+++ components/features/student/schema.ts
@@ -13,11 +13,38 @@ export const formSchema = z.object({
 export type FormValues = z.infer<typeof formSchema>;
 
 export const getStudentSchema = (customFields: CustomFields[]) => {
-    const schema = formSchema.extend({
-        customFields: z.object({}),
+    const dynamicFields: Record<string, z.ZodTypeAny> = {};
+
+    customFields.forEach((field) => {
+        let fieldSchema: z.ZodTypeAny;
+
+        switch (field.type) {
+            case "string":
+                fieldSchema = field.required
+                    ? z.string()
+                    : z.string().optional().default(field.defaultValue as string);
+                break;
+            case "number":
+                fieldSchema = field.required
+                    ? z.number()
+                    : z.number().optional().default(Number(field.defaultValue));
+                break;
+            case "boolean":
+                fieldSchema = field.required
+                    ? z.boolean()
+                    : z.boolean().optional().default(Boolean(field.defaultValue));
+                break;
+            case "date":
+                fieldSchema = field.required ? z.date() : z.date().optional();
+                break;
+            default:
+                fieldSchema = z.any();
+        }
+
+        dynamicFields[field.name] = fieldSchema;
     });
 
-    customFields.forEach((field) => {});
+    const extendedSchema = formSchema.extend(dynamicFields);
 
-    return schema;
+    return extendedSchema;
 };
---
+++ prisma/migrations/20241115060425_custom_field_default_value/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "CustomFields" ADD COLUMN     "defaultValue" TEXT DEFAULT '';

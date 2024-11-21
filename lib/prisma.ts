02d68fca9f7a7b9410cf59e106c177d5b00c57d5
Ivan Lara <ukikalix@gmail.com>
2024-11-20 19:24:56 -0500

    ft/global filter added in order to filter some schemas by status

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,34 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
+
+const statusGlobalFilter = ["Teacher", "Student", "Parent", "Announcement"];
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
+    return new PrismaClient();
+};
 
 declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+    prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export default prisma
+prisma.$extends({
+    query: {
+        $allModels: {
+            async findMany({ model, args, query }) {
+                if (statusGlobalFilter.includes(model)) {
+                    if (!args) args = {};
+                    if (!args.where) args.where = {};
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+                    args.where.AND = [{ status: "ACTIVE" }];
+                }
+
+                return query(args);
+            },
+        },
+    },
+});
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;

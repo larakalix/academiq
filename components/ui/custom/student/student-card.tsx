8630953ff07fe16965e637716160419c56f20c13
Ivan Lara <ukikalix@gmail.com>
2024-11-24 02:30:21 -0500

    fx/add student status in summary

--- components/ui/custom/student/student-card.tsx
+++ components/ui/custom/student/student-card.tsx
@@ -6,7 +6,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "../../avatar";
 import { StudentSummary } from "./student-summary";
 import type { Student } from "@/types/schemas/students";
-import { getInitials } from "@/lib/utils";
+import { cn, getInitials } from "@/lib/utils";
 
 type Props = {
     student: Student;
@@ -16,7 +16,7 @@ export const StudentCard = ({ student }: Props) => {
     return (
         <Card key={student.id}>
             <CardContent className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
-                <figure className="flex justify-center items-center">
+                <figure className="flex justify-center items-center relative">
                     <Avatar className="size-20">
                         <AvatarImage
                             src={student.photo ?? ""}
@@ -26,6 +26,23 @@ export const StudentCard = ({ student }: Props) => {
                             {getInitials(student?.name)}
                         </AvatarFallback>
                     </Avatar>
+
+                    <span
+                        className={cn(
+                            "absolute right-7 bottom-[18px] rounded-full size-3 ring-2 ring-white",
+                            {
+                                "bg-green-500":
+                                    student.studentStatus === "ACTIVE",
+                                "bg-yellow-500":
+                                    student.studentStatus === "SUSPENDED",
+                                "bg-red-500":
+                                    student.studentStatus === "INACTIVE",
+                                "bg-blue-500":
+                                    student.studentStatus === "TRANSFERRED" ||
+                                    student.studentStatus === "GRADUATED",
+                            }
+                        )}
+                    />
                 </figure>
 
                 <div className="md:col-span-2 flex flex-col justify-center items-center md:items-start pl-0 gap-y-1 md:pl-2">
